099f20ac9e2e9779402baf8bb320831de523fa55
shubhampatil0001 <sghorband38@gmail.com>
2026-08-10 00:09:54 +0530

    Day 2 - Variables and Data Types

---
+++ practice.py
@@ -0,0 +1,42 @@
+#storing value in variable and printing 
+name="SHUBHAM KESHAVRAO GHORBAND"
+age= 19
+branch="BSC(AI&IOT)"
+percentage= 80.5
+
+print("NAME:", name)
+print("AGE:", age)
+print("BRANCH:", branch)
+print("PERCENTAGE:", percentage)
+
+
+#finding data type using type functon
+a=25
+b=15.5
+c="python"
+d=True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+#converting string into integer and sum 50
+a="100"
+print(int(a)+50)
+
+
+
+#creating 3 variables in one line
+name, age, city = "Shubham" , 18 ,"Nanded"
+
+
+#swapping variables 
+a = 10
+b = 20
+
+t = a
+a = b
+b = t
+
+print("After swapping: a =", a)
+print("After swapping: b =", b)
---
+++ student_information.py
@@ -0,0 +1,20 @@
+name = "SHUBHAM PATIL"
+age = 20
+branch = 'BSC AI & IOT'
+percentage = 99.0
+college = "computational science "
+roll_no = "VP21151"
+semester = '2nd'
+city = "NANDED"
+
+print("****************************************")
+print("------STUDENT INFORMATION-------")
+print("****************************************")
+print("NAME:",name)
+print("AGE:", age)
+print("BRANCH:" ,branch)
+print("PERCENTAGE:", percentage)
+print("COLLEGE:", college)
+print("ROLL_NO:",  roll_no)
+print("SEMESTER:", semester)
+print("CITY:", city)
