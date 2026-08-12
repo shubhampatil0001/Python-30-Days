ac647dceb66ca34456f5efb0a2ca82384a86d55f
Shubham Patil <sghorband38@gmail.com>
2026-08-12 23:02:06 +0530

    Day 4 - Operators
    Learned and practiced Arithmetic, Comparison, Assignment, Logical, Identity, and Membership operators in Python. Added a Marks Calculator mini project.

---
+++ marks_calculator.py
@@ -0,0 +1,11 @@
+print("------MARKS CALCULATOR------")
+
+math = float(input("Enter marks of math:"))
+python = float(input("Enter marks of python:"))
+english = float(input("Enter marks of english"))
+
+total = math + python + english 
+percentage = total/3
+
+print("Total Marks:",total)
+print("Percentage:",percentage )
--- practice.py
+++ practice.py
@@ -1,45 +1,29 @@
-# Day 3 - User Input and Type Conversion
-#TASK 1
+# Day 4 - Operators
 
-name = input("Enter your name:")
-print("Hello",name)
+a = 10
+b = 3
 
-age = int(input("Enter your age:"))
-print("Your age is:",age)
+print("Addition:", a + b)
+print("Subtraction:", a - b)#Arithematic operators 
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Remainder:", a % b)
+print("Power:", a ** b)
 
-percentage = float(input("Enter your percentage:"))
-print("Your percentage is:",percentage)
-
-#using type function for checking data type of variable
-
-print(type(name))
-print(type(age))
-print(type(percentage))
-
-
-
-#TASK 2
-#Taking data from the user of name and city
-name = str(input("Enter your name:"))
-city = str(input("Enter your city:"))
+print("Equal:", a == b)
+print("Not Equal:", a != b) #comparison operator
+print("Greater:", a > b)
+print("Less:", a < b)
 
 
+#checking age using opertaorss  statemet 
+age = float(input("Enter your age:"))
+print( age >= 18 )
 
-#TASK 3
-#taking 2 intergers from the user and performing arithmetic operations
-a = int(input("Enter a first integer:"))
-b = int(input("Enter a second :"))
-print("Addition:",a+b)
-print("Subtraction:",a-b)
-
-
-#length and width of rectangle
-length = float(input("Enter length of rectangle:"))
-width = float(input('Enter width of rectangle:'))
-area = length * width
-print("Area of rectangle is:",area)
-
-
-#finding dattype of celsius 
-temprature = float(input("Enter temperature in celsius:"))
-print("Data type of temperature:", type(temprature))
+#checkinbg odd or even number using if else statement
+num = int(input("Enter a number:"))
+if num % 2 == 0:
+    print(num, "is an even number")
+else :
+    print(num, "is an odd number")
