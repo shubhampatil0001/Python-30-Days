5425cce0736efc312c7e7f5a9627e38e41800b81
Shubham Patil <sghorband38@gmail.com>
2026-08-19 09:02:00 +0530

    Create practice.py for fruit tuple operations
    Added a script to demonstrate tuple operations with fruits.Learned Python tuples and practiced indexing, slicing, len(), count(), and index().

---
+++ Day-09/practice.py
@@ -0,0 +1,15 @@
+fruits = ("Apple", "Banana" , "Mango", "Apple")
+
+print(fruits)
+print("First fruit:",fruits[0]) #using index\
+    
+print("Last fruit:", fruits[-1]) # using negative index
+
+
+print ("Slicing:",fruits[1:3]) #to extract multiple items
+
+print("Total items:",len(fruits)) # usin len() no of items
+
+print("Apple count:",fruits.count("Apple")) # counting items using count()
+
+print("Banana index:",fruits.index("Banana")) #finding index no using index()
