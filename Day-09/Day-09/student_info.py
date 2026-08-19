049e8ee744d09f32874ca157eace50169b0c20c2
Shubham Patil <sghorband38@gmail.com>
2026-08-19 09:03:35 +0530

    Add student_info.py to display student details
    Learned Python tuples, indexing, negative indexing, slicing, immutability, len(), count(), and index(). Added a Student Information mini project.

---
+++ Day-09/Day-09/student_info.py
@@ -0,0 +1,15 @@
+student = ("Shubham" , 18 , "Engineering" , 85.5 )
+
+print("Name :", student[0])
+print("Age :",student[1])
+print("Percentage",student[-1])
+print("Course:",student[-2])
+
+info = ("NANDED" , "MAHARASHTRA", "INDIA")
+
+print("CITY:",info[0])
+print("STATE :",info[1])
+print("COUNTRY :", info[2])
+
+
+print("*********** THANK YOU **************")
