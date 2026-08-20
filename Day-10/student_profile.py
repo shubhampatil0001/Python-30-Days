670723c7b2ba2e0219d7b032184ac5386525766a
Shubham Patil <sghorband38@gmail.com>
2026-08-20 20:55:08 +0530

    Add Student Profile Project
    Added a mini project using Python dictionaries to store and display student information.

---
+++ Day-10/student_profile.py
@@ -0,0 +1,13 @@
+print("________ WELCOME _________")
+
+student = {
+    "name":    input("Enter your name: "),
+    "age": int(input("Enter your age: ")),
+    "city":    input("Enter your city :"),
+    "course":  input("Enter your course :"),
+    "roll no": int(input("Enter your roll no :")),
+    "mobile no":int(input("Enter your mobile no:"))
+}
+print("STUDENT PROFILE :",student)
+
+print("------ THANK YOU -------")
