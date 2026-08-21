1707412d96e74f809c88e0b28d18b2357b0a8882
Shubham Patil <sghorband38@gmail.com>
2026-08-21 23:10:39 +0530

    Implement unique student names tracking
    Add functionality to track unique student names.

---
+++ Day-11/unique_students.py
@@ -0,0 +1,11 @@
+names = ["shubham","pavan","swapnil","shubham","swapnil"]
+new_name = set(names)
+print("Unique Students :",new_name)
+
+new_name.add("shahaji")
+
+print("shivkumar" in new_name)
+
+print("Total Unique names :",new_name)
+
+print(len(new_name))
