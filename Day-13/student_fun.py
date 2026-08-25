e601d33d41566b2934b6ec488ab8a96c17621193
Shubham Patil <sghorband38@gmail.com>
2026-08-25 21:16:50 +0530

    Add student functions and welcome message

---
+++ Day-13/student_fun.py
@@ -0,0 +1,22 @@
+def welcome():
+    print("welcome to student system")
+    
+    
+def show_student():
+    print(" Student Name : shubham")
+    print(".   Surname : Patil")
+
+
+def show_course():
+    print("Course name : Engineering")
+    
+
+def love():
+    print("Thank you so much .....")    
+    
+           
+
+welcome()
+show_student()
+show_course()
+love()           
