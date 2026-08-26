3858c9f1ece4dc888bd48fcbec1d92aca8dfaba4
Shubham Patil <sghorband38@gmail.com>
2026-08-26 09:02:28 +0530

    Add student_info function to display student details
    Learned Python function parameters and arguments, including single and multiple parameters, positional argument order, and reusable functions. Added a Student Details mini project.

---
+++ Day-14/student_detail.py
@@ -0,0 +1,10 @@
+def student_info(name,age,city,course,number):
+    print("NAME :" ,name)
+    print("AGE :",age)
+    print("CITY :",city)
+    print("COURSE:",course)
+    print("NUMBER :",number)
+    
+student_info("SHUBHAM",18,"NANDED","ENGINEERING",985647889)
+student_info("SHIVKUMAR",19,"HINGOLI","BSC",8674893938)
+student_info("PAVAN",21,"PARBHANI","SUGAR ALCOHOL",9856473838)
