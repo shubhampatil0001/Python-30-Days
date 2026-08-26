fe19d5a0b25b7b70cf1d5faad88eba744a264073
Shubham Patil <sghorband38@gmail.com>
2026-08-26 09:00:45 +0530

    Add functions to display greetings, age, and results
    Learned Python function parameters and arguments, including single and multiple parameters, positional argument order, and reusable functions. Added a Student Details mini project.

---
+++ Day-14/parameters_arguments.py
@@ -0,0 +1,54 @@
+# Function and parameters and Arguments
+
+def greet(name) :
+    print("Hello",name)
+    
+greet("SHUBHAM")
+greet("RAHUL")    
+
+def show_age(age):
+    print("AGE :",age)
+    
+show_age("18")
+show_age("19")
+show_age("12")    
+
+
+def show_info(name,age):
+    print("NAME :",name)
+    print("AGE :", age)
+show_info ("shubham","18")
+
+# welcome 
+def welcome(name):
+    print("WELCOME",name)
+
+welcome("SHUBHAM") 
+welcome("RAHUL")   
+
+#product
+def show_product(product,price):
+    print("PRODUCT NAME :",product)
+    print("PRODUCT PRICE : ",price)
+    
+show_product("DERMA FACE WASH", 450)    
+show_product("NO 1 SOAP", 250)
+
+# name and marks
+def show_info(name,marks):
+    print("NAME :",name)
+    print("MARKS :",marks)
+     
+show_info("SHUBHAM",18)     
+show_info("SWARAj",12)
+
+
+#Student result display 
+def result(name, subject, marks):
+    print("NAME :",name)
+    print("SUBJECT :",subject)
+    print("MARKS :",marks)
+    
+result("SHUBHAM","PYTHON",239) 
+result("CHAITNYA","C LANGUAGE",234)
+result("YASH","JAVA",249)    
