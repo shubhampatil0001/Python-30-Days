6fcbf8582fa408c425065141bc8aa1975b846840
Shubham Patil <sghorband38@gmail.com>
2026-08-30 11:16:13 +0530

    Add student information system in college_proj.py
    Learned Python variable scope, local variables, global variables, and the global keyword. Practiced accessing variables inside and outside functions and created a Student Information practice program.

---
+++ Day-16/college_proj.py
@@ -0,0 +1,15 @@
+# Student Information System
+
+
+college = " SRTMUN UNIVERSITY"
+
+def show_college():
+    print("COLLEGE :",college)
+    
+def show_student(name,age):
+    print("STUDENT:",name)
+    print("AGE  :",age) 
+    
+show_college()
+show_student("Shubham", 29)    
+       
