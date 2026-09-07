c0767578f681782cad382cdbd83f0c5af4e62a4b
Shubham Patil <sghorband38@gmail.com>
2026-09-07 21:54:10 +0530

    Create student_class_obj.py

---
+++ Day-20/student_class_obj.py
@@ -0,0 +1,26 @@
+# creating a 2 objects using class
+
+class student:
+    pass
+
+student1 = student()
+
+student1.name = "Shubham"
+student1.age = 19
+student1.city = "Nanded"
+
+student2 = student ()
+
+student2.name = "Dipali"
+student2.age = 19
+student2.city = "Parbhani"
+
+print("Student -- 1")
+print("Name :",student1.name)
+print("Age :", student1.age)
+print("City :",student1.city)
+
+print("Student -- 2")
+print("Name :",student2.name)
+print("Age :", student2.age)
+print("City :",student2.city)
